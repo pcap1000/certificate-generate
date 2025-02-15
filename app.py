54fc62be60ceebfd4da39baeecd4d3509091b0d7
pcap <praneelchettys@gmail.com>
2025-02-15 22:55:31 +0530

    commit for Fix 1

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def generate():
         return str(e), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- maincode.py
+++ maincode.py
@@ -12,8 +12,8 @@ import math
 from fpdf import FPDF
 
 # Register custom fonts for Coursera certificates
-pdfmetrics.registerFont(TTFont('RivalRegular', r'C:\Users\prane\Downloads\certificategenerator\certificate_generator\fonts\rival-regular-1.otf'))
-pdfmetrics.registerFont(TTFont('DateFont', r'C:\Users\prane\Downloads\certificategenerator\certificate_generator\fonts\URW Grotesk Wide Light Oblique.ttf'))
+pdfmetrics.registerFont(TTFont('RivalRegular', r'fonts\rival-regular-1.otf'))
+pdfmetrics.registerFont(TTFont('DateFont', r'fonts\URW Grotesk Wide Light Oblique.ttf'))
 
 def generate_certificate(name, date, course, selected_company, course_provider="Coursera", instructor_name=None, logo_path=None):
     if course_provider == "Coursera":
@@ -23,7 +23,7 @@ def generate_certificate(name, date, course, selected_company, course_provider="
 
         # Draw border image
         try:
-            border_path = r"E:\Projects\certificate_generator_one\static\Border.png"
+            border_path = r"static\Border.png"
             border_img = ImageReader(border_path)
             c.drawImage(border_img, 0, 0, width=height, height=width)
         except Exception as e:
@@ -46,7 +46,7 @@ def generate_certificate(name, date, course, selected_company, course_provider="
             second_line = "through Coursera."
             c.drawString(x, y - 12, second_line)
             try:
-                logo_path_left = r"C:\Users\prane\Downloads\certificategenerator\certificate_generator\static\purepng.com-ibm-logologobrand-logoiconslogos-251519939176ka7y8.png"
+                logo_path_left = r"static\purepng.com-ibm-logologobrand-logoiconslogos-251519939176ka7y8.png"
                 img_left = ImageReader(logo_path_left)
                 l_width = 2.0*inch
                 aspect_left = img_left.getSize()[1] / img_left.getSize()[0]
@@ -64,7 +64,7 @@ def generate_certificate(name, date, course, selected_company, course_provider="
             second_line = "offered through Coursera."
             c.drawString(x, y - 12, second_line)
             try:
-                logo_path_left = r"C:\Users\prane\Downloads\certificategenerator\certificate_generator\static\Johns_Hopkins_University_Logo-1536x258.png"
+                logo_path_left = r"static\Johns_Hopkins_University_Logo-1536x258.png"
                 img_left = ImageReader(logo_path_left)
                 l_width = 3.5*inch
                 aspect_left = img_left.getSize()[1] / img_left.getSize()[0]
@@ -82,7 +82,7 @@ def generate_certificate(name, date, course, selected_company, course_provider="
             second_line = "offered through Coursera."
             c.drawString(x, y - 12, second_line)
             try:
-                logo_path_left = r"C:\Users\prane\Downloads\certificategenerator\certificate_generator\static\duke_university.jpg"
+                logo_path_left = r"static\duke_university.jpg"
                 img_left = ImageReader(logo_path_left)
                 l_width = 2.0*inch
                 aspect_left = img_left.getSize()[1] / img_left.getSize()[0]
@@ -100,7 +100,7 @@ def generate_certificate(name, date, course, selected_company, course_provider="
             second_line = "offered through Coursera."
             c.drawString(x, y - 12, second_line)
             try:
-                logo_path_left = r"C:\Users\prane\Downloads\certificategenerator\certificate_generator\static\Wharton_Logo_Small.png"
+                logo_path_left = r"static\Wharton_Logo_Small.png"
                 img_left = ImageReader(logo_path_left)
                 l_width = 3.5*inch
                 aspect_left = img_left.getSize()[1] / img_left.getSize()[0]
@@ -118,7 +118,7 @@ def generate_certificate(name, date, course, selected_company, course_provider="
             second_line = "offered through Coursera."
             c.drawString(x, y - 12, second_line)
             try:
-                logo_path_left = r"C:\Users\prane\Downloads\certificategenerator\certificate_generator\static\yonsei.png"
+                logo_path_left = r"static\yonsei.png"
                 img_left = ImageReader(logo_path_left)
                 l_width = 3.5*inch
                 aspect_left = img_left.getSize()[1] / img_left.getSize()[0]
@@ -136,7 +136,7 @@ def generate_certificate(name, date, course, selected_company, course_provider="
             second_line = "offered through Coursera."
             c.drawString(x, y - 12, second_line)
             try:
-                logo_path_left = r"C:\Users\prane\Downloads\certificategenerator\certificate_generator\static\eidenburgh.png"
+                logo_path_left = r"static\eidenburgh.png"
                 img_left = ImageReader(logo_path_left)
                 l_width = 3.9*inch
                 aspect_left = img_left.getSize()[1] / img_left.getSize()[0]
@@ -154,7 +154,7 @@ def generate_certificate(name, date, course, selected_company, course_provider="
             second_line = "offered through Coursera."
             c.drawString(x, y - 12, second_line)
             try:
-                logo_path_left = r"C:\Users\prane\Downloads\certificategenerator\certificate_generator\static\university_of_london.jpeg"
+                logo_path_left = r"static\university_of_london.jpeg"
                 img_left = ImageReader(logo_path_left)
                 l_width = 1.5*inch
                 aspect_left = img_left.getSize()[1] / img_left.getSize()[0]
@@ -164,7 +164,7 @@ def generate_certificate(name, date, course, selected_company, course_provider="
                 print(f"Could not load left logo: {e}")
 
         try:
-            logo_path_right = r"C:\Users\prane\Downloads\certificategenerator\certificate_generator\static\Coursera-MZYLUZ38AX27-1536x1187.jpg"
+            logo_path_right = r"static\Coursera-MZYLUZ38AX27-1536x1187.jpg"
             img_right = ImageReader(logo_path_right)
             r_width = 2.5*inch
             aspect_right = img_right.getSize()[1] / img_right.getSize()[0]
@@ -175,7 +175,7 @@ def generate_certificate(name, date, course, selected_company, course_provider="
 
         # Signature
         try:
-            signature_path = r"C:\Users\prane\Downloads\certificategenerator\certificate_generator\static\sign.png"
+            signature_path = r"static\sign.png"
             img_signature = ImageReader(signature_path)
             sig_width = 2.0 * inch
             aspect_ratio = img_signature.getSize()[1] / img_signature.getSize()[0]
@@ -273,9 +273,9 @@ def generate_certificate(name, date, course, selected_company, course_provider="
         pdf.add_page()
 
         # Add custom fonts
-        pdf.add_font("Gunaydin", style="", fname=r"C:\Users\prane\Downloads\certificate_generator_one\certificate_generator\fonts\Gunaydin-Regular.ttf", uni=True)
-        pdf.add_font("TimesNewRomanBold", style="", fname=r"C:\Users\prane\Downloads\certificate_generator_one\certificate_generator\fonts\times-new-roman-bold.otf", uni=True)
-        pdf.add_font("Touche", style="", fname=r"C:\Users\prane\Downloads\certificate_generator_one\certificate_generator\fonts\Touche-Semibold-BF642a2ebf682d9.otf", uni=True)
+        pdf.add_font("Gunaydin", style="", fname=r"fonts\Altone Trial-Regular.ttf", uni=True)
+        pdf.add_font("TimesNewRomanBold", style="", fname=r"fonts\times-new-roman-bold.otf", uni=True)
+        pdf.add_font("Touche", style="", fname=r"fonts\Touche-Semibold-BF642a2ebf682d9.otf", uni=True)
 
         page_width = pdf.w
         page_height = pdf.h
