3cedf4c5062ae1122aa1224ac6a59d588f3d0206
pcap <praneelchettys@gmail.com>
2025-02-16 21:36:14 +0530

    28

--- maincode.py
+++ maincode.py
@@ -18,9 +18,9 @@ from fpdf import FPDF
 pdfmetrics.registerFont(TTFont('RivalRegular', 'fonts/rival-regular-1.otf'))
 pdfmetrics.registerFont(TTFont('DateFont', 'fonts/URW Grotesk Wide Light Oblique.ttf'))
 # Register fonts for Udemy certificates
-pdfmetrics.registerFont(TTFont('Gunaydin', 'fonts/Gunaydin-Regular.ttf'))
-pdfmetrics.registerFont(TTFont('TimesNewRomanBold', 'fonts/times-new-roman-bold.otf'))
-pdfmetrics.registerFont(TTFont('Touche', 'fonts/Touche-Semibold-BF642a2ebf682d9.ttf'))
+# pdfmetrics.registerFont(TTFont('Gunaydin', 'fonts/Gunaydin-Regular.ttf'))
+# pdfmetrics.registerFont(TTFont('TimesNewRomanBold', 'fonts/times-new-roman-bold.otf'))
+# pdfmetrics.registerFont(TTFont('Touche', 'fonts/Touche-Semibold-BF642a2ebf682d9.ttf'))
 
 def generate_certificate(name, date, course, selected_company, course_provider="Coursera", instructor_name=None, logo_path=None):
     if course_provider == "Coursera":
@@ -284,9 +284,9 @@ def generate_certificate(name, date, course, selected_company, course_provider="
         # pdfmetrics.registerFont(TTFont('Touche', 'fonts/Touche-Semibold-BF642a2ebf682d9.ttf'))
                 
 
-        pdf.add_font("Gunaydin", style="", fname="fonts/Gunaydin-Regular.ttf", uni=True)
-        pdf.add_font("TimesNewRomanBold", style="", fname="fonts/times-new-roman-bold.otf", uni=True)
-        pdf.add_font("Touche", style="", fname="fonts/Touche-Semibold-BF642a2ebf682d9.ttf", uni=True)
+        pdf.add_font("Gunaydin", style="", fname="./fonts/Gunaydin-Regular.ttf", uni=True)
+        pdf.add_font("TimesNewRomanBold", style="", fname="./fonts/times-new-roman-bold.otf", uni=True)
+        pdf.add_font("Touche", style="", fname="./fonts/Touche-Semibold-BF642a2ebf682d9.ttf", uni=True)
 
 
         # # Add custom fonts
