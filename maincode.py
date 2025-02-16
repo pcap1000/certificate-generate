3c7162113ec04a0ff711e802a6d9e562cfd1d427
pcap <praneelchettys@gmail.com>
2025-02-16 14:45:53 +0530

    21

--- maincode.py
+++ maincode.py
@@ -12,8 +12,14 @@ import math
 from fpdf import FPDF
 
 # Register custom fonts for Coursera certificates
-pdfmetrics.registerFont(TTFont('RivalRegular', r'fonts/rival-regular-1.otf'))
-pdfmetrics.registerFont(TTFont('DateFont', r'fonts/URW Grotesk Wide Light Oblique.ttf'))
+# pdfmetrics.registerFont(TTFont('RivalRegular', r'fonts/rival-regular-1.otf'))
+# pdfmetrics.registerFont(TTFont('DateFont', r'fonts/URW Grotesk Wide Light Oblique.ttf'))
+# Register fonts once at the module level
+pdfmetrics.registerFont(TTFont('RivalRegular', 'fonts/rival-regular-1.otf'))
+pdfmetrics.registerFont(TTFont('DateFont', 'fonts/URW Grotesk Wide Light Oblique.ttf'))
+pdfmetrics.registerFont(TTFont('Gunaydin', 'fonts/Gunaydin-Regular.ttf'))
+pdfmetrics.registerFont(TTFont('TimesNewRomanBold', 'fonts/times-new-roman-bold.otf'))
+pdfmetrics.registerFont(TTFont('Touche', 'fonts/Touche-Semibold-BF642a2ebf682d9.ttf'))
 
 def generate_certificate(name, date, course, selected_company, course_provider="Coursera", instructor_name=None, logo_path=None):
     if course_provider == "Coursera":
@@ -273,9 +279,9 @@ def generate_certificate(name, date, course, selected_company, course_provider="
         pdf.add_page()
 
 
-        pdfmetrics.registerFont(TTFont('Gunaydin', r'fonts/Gunaydin-Regular.ttf'))
-        pdfmetrics.registerFont(TTFont('TimesNewRomanBold', r'fonts/times-new-roman-bold.otf'))
-        pdfmetrics.registerFont(TTFont('Touche', r'fonts/Touche-Semibold-BF642a2ebf682d9.ttf'))
+        # pdfmetrics.registerFont(TTFont('Gunaydin', r'fonts/Gunaydin-Regular.ttf'))
+        # pdfmetrics.registerFont(TTFont('TimesNewRomanBold', r'fonts/times-new-roman-bold.otf'))
+        # pdfmetrics.registerFont(TTFont('Touche', r'fonts/Touche-Semibold-BF642a2ebf682d9.ttf'))
 
         # # Add custom fonts
         # pdf.add_font("Gunaydin", style="", fname=r"fonts/Gunaydin-Regular.ttf", uni=True)
